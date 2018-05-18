d592fa5d5b4dd1a96465de5c2f5551fe62d122e5
twoshoe <twoshoe@gmail.com>
2018-05-17 23:36:45 -0400

    Final version for the night.

--- kaeseya.py
+++ kaeseya.py
@@ -3,8 +3,9 @@
 import argparse
 import logging
 import requests
+import json
 
-API_URL = http://bms.kaseya.com/api/
+API_URL = 'http://bms.kaseya.com/api/'
 AUTH = {
        'grant_type=password'
        'username='
@@ -12,20 +13,22 @@ AUTH = {
        'tenant='
         }
 
-def main:
+def main():
 
-    auth_reponse = requests.post('{0}token'.format(API_URL),
-        data=AUTH)
+    #auth_reponse = requests.post('{0}token'.format(API_URL),
+    #    data=AUTH)
 
-    bearer = { 'Authorization': auth_response.json() }
+   # bearer = { 'Authorization': auth_response.json() }
 
-    headers=bearer
-    location_response = requests.post('{0}import/AccountLocations'.format(API_URL),
-        json=AccountLocations)
+    #Read locations from file
+    with open('ocean_blue.json') as json_data:
+        AccountLocations = json.load(json_data)
+        print(json.dumps(AccountLocations, sort_keys=True, indent=4))
 
-
-
-
+        #Post Locations
+       #location_response = requests.post('{0}import/AccountLocations'.format(API_URL),
+       #     json=AccountLocations,headers=bearer)
+    
 
 if __name__ ==  "__main__":
     main()
