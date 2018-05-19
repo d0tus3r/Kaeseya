6308055401ef7820568b113ea65aaf45faf30194
d0tus3r <d0tus3r@gmail.com>
2018-05-18 21:09:37 -0400

    adds location json importing

--- bms_locations.py
+++ bms_locations.py
@@ -11,7 +11,8 @@ PASSWORD = CONFIG.get('BMS', 'PASSWORD')
 GRANT_TYPE = CONFIG.get('BMS', 'GRANT_TYPE')
 TENANT = CONFIG.get('BMS', 'TENANT')
 API_URL = CONFIG.get('BMS', 'API_URL')
-
+LOCATION_URL = CONFIG.get('BMS', 'LOCATION_URL')
+#for initial login
 HEADERS = {
     'content-type': 'application/x-www-form-urlencoded',
     'accept': 'application/json'
@@ -29,9 +30,25 @@ def main():
     '''Logs into BMS by Kaseya and adds locations (stored as json) to an account'''
     auth_response = requests.post(API_URL, headers=HEADERS, data=PARAMETERS)
 
-    bearer = {'Authorization': auth_response.json()}
+    bearer = auth_response.json()
+    token = bearer['access_token']
+    #print(token)
 
-    print(bearer)
+
+    #Read locations from file
+    with open('forest_dunes.json') as json_data:
+        AccountLocations = json.load(json_data)
+        print(json.dumps(AccountLocations, sort_keys=True, indent=4))
+
+        location_header = {
+            'accept': 'application/json',
+            'content-type': 'application/json',
+            'authorization': token
+        }
+       #Post Locations
+        location_response = requests.post(LOCATION_URL, json=AccountLocations, headers=location_header)
+
+        print(location_response)
 
 
 if __name__ == "__main__":
